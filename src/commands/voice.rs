5ec6731db617f621279d4b80a7a2afee370ca85a
Eva G. <orakle@tutamail.com>
2025-11-08 16:32:49 +0100

    feat: auto-connect if play used without connecting before

--- src/commands/voice.rs
+++ src/commands/voice.rs
@@ -10,25 +10,7 @@ use crate::types::playground::Playground;
 
 #[poise::command(slash_command, prefix_command, guild_only)]
 pub async fn connect(ctx: Context<'_>) -> Result<(), Error> {
-    let playground = Playground::from(ctx);
-
-    let channel_id = match playground.channel_id {
-        Some(id) => id,
-        None => {
-            ctx.reply("You are not in a voice channel.").await?;
-            return Ok(());
-        }
-    };
-
-    let manager = &ctx.data().songbird;
-
-    if let Ok(handler_lock) = manager.join(playground.guild_id, channel_id).await {
-        ctx.reply("Joined!").await?;
-        let mut handler = handler_lock.lock().await;
-        handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier)
-    }
-
-    Ok(())
+    connect_to_channel_from_ctx(ctx).await
 }
 
 #[poise::command(slash_command, prefix_command, guild_only)]
@@ -56,14 +38,15 @@ pub async fn leave(ctx: Context<'_>) -> Result<(), Error> {
 pub async fn play(ctx: Context<'_>, url: String) -> Result<(), Error> {
     let playground = Playground::from(ctx);
 
-    let answer = ctx.say("searching...").await?;
-
     let do_search = !url.starts_with("http");
-
     let data = ctx.data();
-
     let manager = &data.songbird;
 
+    if manager.get(playground.guild_id).is_none() {
+        connect_to_channel_from_ctx(ctx).await?;
+    }
+
+    let answer = ctx.say("searching...").await?;
     if let Some(handler_lock) = manager.get(playground.guild_id) {
         let mut handler = handler_lock.lock().await;
 
@@ -98,6 +81,7 @@ pub async fn play(ctx: Context<'_>, url: String) -> Result<(), Error> {
 }
 
 #[poise::command(prefix_command, guild_only)]
+#[poise::command(slash_command, prefix_command, guild_only)]
 pub async fn skip(ctx: Context<'_>) -> Result<(), Error> {
     let playground = Playground::from(ctx);
 
